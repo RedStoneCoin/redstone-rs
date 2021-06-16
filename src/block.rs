56951906ab465f109ea500a0fefd5eade3af49a4
LeoCornelius <leocornelius019@gmail.com>
2021-06-16 16:34:35 +0100

    Start validation code

--- src/block.rs
+++ src/block.rs
@@ -1,4 +1,4 @@
-use crate::{crypto::Hashable, transaction::Transaction};
+use crate::{blockchain::Blockchain, crypto::Hashable, executable::Executable, state::GlobalState, transaction::Transaction};
 
 pub struct Header {
     height: u64,
@@ -10,12 +10,14 @@ pub struct Header {
     proposer: String, // the publickey of the proposer
     transactions_merkle_root: String,
     header_payload: u8,
+    proof: String, // The vrf proof of the proposer as hex
+    proposer_signature: String, // proposers signature
+    validator_signatures: Vec<String>
 }
 pub struct Block {
     hash: String,
     header: Header,
     transactions: Vec<String>,
-    signature: String, // proposers signature
 }
 impl Hashable for Header {
     fn bytes(&self) -> Vec<u8> {
@@ -41,5 +43,54 @@ impl Block {
     }
     pub fn add_txn(&mut self, txn: &Transaction) {
         self.transactions.push(txn.hash.to_owned());
+    }
+}
+
+impl Executable for Block {
+    fn execute(&self, context: &String) -> Result<String, Box<dyn std::error::Error>> {
+        todo!()
+    }
+
+    fn evalute(&self) -> Result<(), Box<dyn std::error::Error>> {
+        // check the hash of this block
+        if self.hash != self.hash_item() {
+            return Err("Hash invalid".into());
+        }
+        // check the chain exists
+        if let Ok(bc) = Blockchain::load(self.header.chain) {
+            if self.header.height == 0 {
+                // if this block is the genesis block of this chain, the parent block should contain a create chain TXN
+                // TODO: Get parent block and look for this txn
+            } else {
+                // check if the height and parent hash is correct on the specified chain
+                let chain_tip = bc.tip();
+                if self.header.parent_hash != chain_tip {
+                    return Err("Invalid parent hash".into());
+                } else {
+                    // get the parent block and check its height is self.header.height - 1
+                    if let Ok(block) = Block::get(chain_tip) {
+                        if block.header.height != self.header.height - 1 {
+                            return Err("Height missmatch".into());
+                        } else {
+                            // check the state hash
+                            if self.header.state_hash != GlobalState::current() {
+                                return Err("Invalid state hash".into())
+                            }
+                            // TODO: Check uncle root (the merkle root of all the tips of the other chains)
+                            // TODO: Check the proposer
+                        }
+                    }
+                }
+            }
+        } else {
+            return Err("Chain does not exist".into());
+        }
+        Ok(())
+    }
+
+    /// # Cost
+    /// Not used, will panic if called
+    fn cost(&self, context: &String) -> u64 {
+        unimplemented!()
     }
 }
