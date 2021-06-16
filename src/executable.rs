0567e3f987ff2f97832ea9cb7f2d2af260a2dbba
LeoCornelius <leocornelius019@gmail.com>
2021-06-16 16:35:07 +0100

    Change executable trait signature

--- src/executable.rs
+++ src/executable.rs
@@ -1,5 +1,5 @@
 pub trait Executable {
     fn execute(&self, context: &String) -> Result<String, Box<dyn std::error::Error>>;
-    fn evalute(&self) -> String; // is this valid executable
+    fn evalute(&self) -> Result<(), Box<dyn std::error::Error>>; // is this valid executable
     fn cost(&self, context: &String) -> u64; // returns the expected cost in microstones
 }
