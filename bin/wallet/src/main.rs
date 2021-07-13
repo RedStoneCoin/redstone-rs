36bb3f4041b92fe9f5aed08eafd36b49a9614fb9
toni <toni@pop-os.localdomain>
2021-07-13 18:39:59 +0200

    Added changes to the wallet

--- bin/wallet/src/main.rs
+++ bin/wallet/src/main.rs
@@ -1,6 +1,14 @@
 use redstone_rs::*;
-fn main() {
-    println!("Starting redstone wallet");
+fn genaddr() {
     let wallet = redstone_rs::keypair::Keypair::generate();
-    println!("{:#?}, {}", wallet, wallet.address())
+    println!("Your wallet address:{}", wallet.address());
+    println!("Private key:{}", &wallet.private_key.to_string());
+}
+fn main() {
+
+    println!("Starting redstone wallet");
+    genaddr();
+   
+
+
 }
